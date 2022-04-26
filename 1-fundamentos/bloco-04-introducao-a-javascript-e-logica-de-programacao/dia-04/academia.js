2595accd8630b33a670dd5c54e783e7b2b0fe03f
Leandro Amaral <leandroba1996@gmail.com>
2022-04-26 18:22:08 -0300

    Subida dos Exercicios atrásados do bloco 4 e inicio do bloco 6

---
+++ 1-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04/academia.js
@@ -0,0 +1,15 @@
+/* Crie uma função que, dado um array de números inteiros, retorne a quantidade de números pares e ímpares no formato: 
+{
+ pares: 0, 
+ ímpares: 0 
+} 
+*/
+
+let numeros = [3, 5, 2, 20, 40, 55]
+
+function paresImpares (valores) {
+    for (let index = 0; index < valores.length; index +=1) {
+        if ()
+    }
+}
+paresImpares ()
